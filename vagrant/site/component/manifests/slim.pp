907e14e64ae489e7a4b6a10fe466fd11f026c71a
Simon Waibl <simon.waibl@mayflower.de>
2015-06-23 15:28:51 +0200

    Revert "add docroot parameter to component::slim"
    This reverts commit 7805d102727135dc9dff3fbe3a9ddda5c6225d88.
    This also adds the relevant fix for the docroot.

--- vagrant/site/component/manifests/slim.pp
+++ vagrant/site/component/manifests/slim.pp
@@ -1,17 +1,11 @@
 class component::slim (
-  $path    = hiera('path', '/var/www/app_name'),
-  $vhost   = hiera('vhost', 'app-name.dev'),
-  $env     = hiera('env', 'dev'),
-  $docroot = undef,
+  $path  = hiera('path', '/var/www/app_name'),
+  $vhost = hiera('vhost', 'app-name.dev'),
+  $env   = hiera('env', 'dev'),
 ) {
 
-  $www_root = $docroot ? {
-    undef => $path,
-    default => "${path}/${docroot}"
-  }
-
   nginx::resource::vhost { $vhost:
-    www_root    => $www_root,
+    www_root    => $path,
     fastcgi     => '127.0.0.1:9000',
     try_files   => ['$uri', '$uri/', '/index.php?$args'],
     location_cfg_append => {
@@ -25,7 +19,7 @@ class component::slim (
   nginx::resource::location{ "${vhost}_static":
     location  => '~ ^/(css|images|js)/',
     vhost     => $vhost,
-    www_root  => $www_root,
+    www_root  => $path,
     try_files => ['$uri', '=404']
   }
 }
